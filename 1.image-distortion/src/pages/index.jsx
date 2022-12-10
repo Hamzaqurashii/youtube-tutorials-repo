4df94892175546ea0d8f32bee362dd1b1c2095ed
Hamzaqurashii <hamzaqurashii1@gmail.com>
2022-12-10 16:07:34 +0500

    aaaa

--- 1.image-distortion/src/pages/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CurtainSlider from "../1.curtain-slider/CurtainSlider";
-import "../styles/home.scss";
-
-const Index = () => {
-  return <CurtainSlider />;
-};
-export default Index;
